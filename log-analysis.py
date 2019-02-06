3424a0d931fa4b089d389c812939c88bf2704328
Martina Masanova <martina.masanova@gmail.com>
2019-02-06 17:56:41 +0000

    style: pycodestyle adjustments

--- log-analysis.py
+++ log-analysis.py
@@ -1,8 +1,12 @@
+#!/usr/bin/env python2.7
+
 import psycopg2
+
 
 def connect_db():
     """ Connect to the news database """
     return psycopg2.connect("dbname=news")
+
 
 def query_view(view):
     db = connect_db()
@@ -14,31 +18,38 @@ def query_view(view):
 
     return results
 
+
 def show_top_three_articles():
     articles = query_view('top_three_articles')
-    text = [ '\nTop three articles of all time are:\n' ]
+    text = ['\nTop three articles of all time are:\n']
     article_no = 1
 
     for article in articles:
-        text.append('{}. {} - {} views'.format(article_no, article[0], str(article[1])))
+        article_info = '{}. {} - {} views'.format(
+            article_no, article[0], str(article[1]))
+        text.append(article_info)
         article_no += 1
 
     print('\n'.join(text))
 
+
 def show_most_popular_authors():
     authors = query_view('most_popular_authors')
-    text = [ '\nThe most popular article authors of all time are:\n' ]
+    text = ['\nThe most popular article authors of all time are:\n']
     author_no = 1
 
     for author in authors:
-        text.append('{}. {} - {} views'.format(author_no, author[0], str(author[1])))
+        author_formatted = '{}. {} - {} views'.format(
+            author_no, author[0], str(author[1]))
+        text.append(author_formatted)
         author_no += 1
 
     print('\n'.join(text))
 
+
 def show_dates_with_many_errors():
     dates = query_view('more_than_one_percent_errors')
-    text = [ '\nOn following dates more than 1% of requests lead to errors:\n' ]
+    text = ['\nOn following dates more than 1% of requests lead to errors:\n']
 
     for date in dates:
         formated_date = date[0].strftime('%B %d, %Y')
@@ -46,10 +57,12 @@ def show_dates_with_many_errors():
 
     print('\n'.join(text))
 
+
 def main():
     show_top_three_articles()
     show_most_popular_authors()
     show_dates_with_many_errors()
     print('\n')
+
 
 main()
